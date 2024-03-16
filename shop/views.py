3d7c435191c3cb726d18f1c7ad20ddce189a65ba
Victoria <viky_lechka@mail.ru>
2024-03-16 16:58:38 +0300

    Home work 3

--- shop/views.py
+++ shop/views.py
@@ -11,7 +11,6 @@ def index(request):
 def user_orders(request, user_id, period):
     user = get_object_or_404(User, pk=user_id)
 
-    # Определяем даты начала и конца периода
     end_date = timezone.now()
     if period == 'week':
         start_date = end_date - timedelta(days=7)
@@ -20,10 +19,8 @@ def user_orders(request, user_id, period):
     elif period == 'year':
         start_date = end_date - timedelta(days=365)
 
-    # Фильтруем заказы по пользователю и времени
     orders = Order.objects.filter(customer=user, date_ordered__range=(start_date, end_date)).order_by('-date_ordered')
 
-    # Получаем уникальные товары из заказов
     unique_products = set()
     for order in orders:
         unique_products |= set(order.products.all())
